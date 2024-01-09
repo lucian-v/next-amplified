1231f8278d5022507de2f24e17c5103554de0de5
Lucian Vasiliu <lucian@thinslices.com>
2024-01-09 15:03:20 +0200

    Auth stuff

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,6 +1,10 @@
 "use client";
+import { Amplify } from 'aws-amplify';
 import type { WithAuthenticatorProps } from "@aws-amplify/ui-react";
 import { withAuthenticator } from "@aws-amplify/ui-react";
+import config from '../../amplifyconfiguration.json';
+
+Amplify.configure(config);
 
 export default withAuthenticator(function Home({
   signOut,
